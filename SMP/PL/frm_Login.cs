1b4b503d5f656ffacc1e3e9bd142ab01cceaa27b
abdullah <abdullah@LAPTOP-0F9G1M2I>
2023-06-01 02:59:41 +0300

    edit

--- SMP/PL/frm_Login.cs
+++ SMP/PL/frm_Login.cs
@@ -16,7 +16,7 @@ namespace SMP.PL
 {
     public partial class frm_Login : Form
     {
-        DB_SMPEntities db =new DB_SMPEntities();
+        DB_SMPEntities db = new DB_SMPEntities();
         User tb_users = new User();
         Main main = new Main();
         frm_Casher frm_casher = new frm_Casher();
@@ -31,9 +31,18 @@ namespace SMP.PL
 
         private void btn_add_Click(object sender, EventArgs e)
         {
+            /*
+admin 
+username: aa
+password: 123
+
+casher
+username: abd
+password; 123
+*/
             Toast toast = new Toast();
             Dialog dialog = new Dialog();
-            if (txt_UserName.Text == ""||txtPass.Text=="")
+            if (txt_UserName.Text == "" || txtPass.Text == "")
             {
                 dialog.Width = this.Width;
                 dialog.lbl_Caption.Text = "Please Complete the information";
@@ -42,20 +51,20 @@ namespace SMP.PL
             else
             {
 
-                    tb_users=db.Users.Where(x=>x.User_Name==txt_UserName.Text&&x.User_Pass==txtPass.Text).FirstOrDefault(); 
-                  if (tb_users != null)
+                tb_users = db.Users.Where(x => x.User_Name == txt_UserName.Text && x.User_Pass == txtPass.Text).FirstOrDefault();
+                if (tb_users != null)
                 {
-                    if(tb_users.User_Roll=="Admin")
+                    if (tb_users.User_Roll == "Admin")
 
                     {
-                    tb_users.User_State = "true";
-                    db.Entry(tb_users).State =EntityState.Modified;
-                    main.lblusername.Text =tb_users.User_Name;
-                    main.lblRoll.Text = tb_users.User_Roll;
-                    main.Enabled = true;
-                    db.SaveChanges();
-                    main.Show();
-                   this.Hide();
+                        tb_users.User_State = "true";
+                        db.Entry(tb_users).State = EntityState.Modified;
+                        main.lblusername.Text = tb_users.User_Name;
+                        main.lblRoll.Text = tb_users.User_Roll;
+                        main.Enabled = true;
+                        db.SaveChanges();
+                        main.Show();
+                        this.Hide();
                     }
                     else
                     {
@@ -68,22 +77,22 @@ namespace SMP.PL
                         frm_casher.Show();
                         this.Hide();
                     }
-                   
+
                 }
-                  else
+                else
                 {
                     MessageBox.Show("Faild Login");
                 }
 
-                              
+
 
 
 
             }
 
 
-        
-    }
+
+        }
 
         private void frm_Login_Load(object sender, EventArgs e)
         {
@@ -92,10 +101,10 @@ namespace SMP.PL
 
         private void btn_colse_Click(object sender, EventArgs e)
         {
-           
-            
-                Environment.Exit(0);
-            
+
+
+            Environment.Exit(0);
+
         }
     }
 }
---
+++ SMP/sqlDB.sql
@@ -0,0 +1,61 @@
+﻿create database DB_SMP
+use DB_SMP
+go
+create table Category
+(
+ID INT PRIMARY KEY IDENTITY(1,1),
+CAT_Name nvarchar(50)  null, 
+Cat_Cover image  null,
+)
+go
+create table Suppliers
+(
+ID INT PRIMARY KEY IDENTITY(1,1),
+Supp_Name nvarchar(50)  null, 
+Supp_Phone nvarchar(50)  null,
+Supp_Email nvarchar(50)  null,
+Supp_image image  null,
+)
+go
+create table Purchases
+(
+ID INT PRIMARY KEY IDENTITY(1,1),
+Cat_id  int  null,
+Supp_id int null,
+Pur_name nvarchar(50)  null,
+Pur_Type nvarchar(50)  null,
+Pur_Cat nvarchar(50)  null,
+Pur_Supp nvarchar(50)  null,
+Pur_Det nvarchar(50)  null,
+Pur_Buy float  null,
+Pur_Sell float  null,
+Pur_Qt float  null,
+Pur_Tsell float  null,
+Pur_Tbuy float null,
+Pur_TRev float  null,
+CONSTRAINT FK_Purchases_Category FOREIGN KEY (Cat_Id) REFERENCES Category(Id),
+    CONSTRAINT FK_Purchases_Suppliers FOREIGN KEY (Supp_Id) REFERENCES Suppliers(Id)
+)
+go
+create table Sells
+(
+ID INT PRIMARY KEY IDENTITY(1,1),
+Sell_nameItem nvarchar(50) null,
+Sell_nameClient nvarchar(50) null,
+Sell_Price float null,
+Sell_Qt float null,
+Sell_TPrice float null,
+Sell_Date datetime null,
+)
+go
+create table Users
+(
+ID INT PRIMARY KEY IDENTITY(1,1),
+User_Name nvarchar(50) null,
+User_Pass nvarchar(50) null,
+User_Roll nvarchar(50) null,
+User_State nvarchar(50) null,
+User_image image  null,
+
+)
+go
